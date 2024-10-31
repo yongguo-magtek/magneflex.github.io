b7523fd83d564ad8e3941340dff09594760fe767
Yong Guo <yongg@magtek.com>
2024-10-31 14:47:18 -0700

    test mtnfc

--- MTNFC.js
+++ MTNFC.js
@@ -1,9 +1,128 @@
 
+(function(exports){
+    var UrlToObject = function (url) {
+        return JSON.parse('{"' + decodeURI(url).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+    };
 
-import {ndefLibrary} from './ndef-lib.js'
+    var  sendNFCCommand = function(command, lastCommand, encrypt) {
+        readerArgument = new MTMagneFlexParameter.readerParameter();
+        readerArgument.timeLimit = '1E';
+        readerArgument.hideUI = true;
+        readerArgument.command = command;
+        readerArgument.lastCommand = lastCommand;
+        readerArgument.encrypt = encrypt;
 
-var  sendNFCCommand = function(command, lastCommand, encrypt) {
-    return new Promise((resolve) =>{
         
-    });
-};
+        requestArgument = new MTMagneFlexParameter.requestParameter();
+        requestArgument.operation = MagneFlexOp.REQUEST_SEND_NFC_COMMAND;
+        requestArgument.httpMethod = HTTPMethod.NONE;
+        requestArgument.closeDeviceAfter = false;
+        requestArgument.destinationURL = "";
+
+        return new Promise((resolve) =>{
+            MTMagneFlexLib.requestSendNFCCommand(readerArgument,requestArgument,function(x){
+                    resolve(UrlToObject(x));
+                });
+        });
+    };
+
+    var startNFCAccess = function() {
+        readerArgument = new MTMagneFlexParameter.readerParameter();
+        readerArgument.timeLimit = '1E';
+        readerArgument.hideUI = true;
+
+        
+        requestArgument = new MTMagneFlexParameter.requestParameter();
+        requestArgument.operation = MagneFlexOp.REQUEST_START_NFC;
+        requestArgument.httpMethod = HTTPMethod.NONE;
+        requestArgument.closeDeviceAfter = false;
+        requestArgument.destinationURL = "";
+
+        return new Promise((resolve) =>{
+            MTMagneFlexLib.requestStartNFC(readerArgument,requestArgument,function (x){
+                resolve(UrlToObject(x));
+            });
+        });
+    };
+
+    var stopNFCAccess = async function() {
+        readerArgument = new MTMagneFlexParameter.readerParameter();
+        readerArgument.timeLimit = '1E';
+        readerArgument.hideUI = true;
+
+        
+        requestArgument = new MTMagneFlexParameter.requestParameter();
+        requestArgument.operation = MagneFlexOp.REQUEST_STOP_NFC;
+        requestArgument.httpMethod = HTTPMethod.NONE;
+        requestArgument.closeDeviceAfter = false;
+        requestArgument.destinationURL = "";
+
+        return new Promise((resolve) =>{
+            MTMagneFlexLib.requestStopNFC(readerArgument,requestArgument,function (x){
+                resolve(UrlToObject(x));
+            });
+        });
+    };
+
+    // read the card and triger reading, readingerror
+    var read = async function () {
+
+    };
+
+    // return a promise for the result of connect device
+    exports.connect = async function( onTagPresented ) {
+        let reader = {
+            scan : async function(option) {
+                if ( typeof option !== undefined && typeof option.signal !== undefined) {
+                    option.signal.onabort = async ()=>{ await stopNFCAccess(); };
+                }
+
+                var nfcStart = await startNFCAccess();
+                if (nfcStart.errorCode == 0) {
+                    read();
+                    return nfcStart;
+                } else {
+                    throw Error(nfcStart.errorMessage);
+                }
+            },
+            disconnect : async function () {
+            }
+        };
+
+        if (typeof onTagPresented === "undefined") {
+            onTagPresented = reader.scan();
+        };
+
+        return new Promise((resolve) =>{
+            MTMagneFlexLib.addEventListener("UserEvent", (userEvent)=>{
+                onTagPresented();
+            }, function(x) {
+                let response = UrlToObject(x);
+                if (response.errorCode == 0) {
+                    resolve(reader);
+                } else {
+                    throw Error(response.errorMessage);
+                }
+            });
+        });
+    };
+    
+
+    // return a promise for the result of sending
+    exports.send = async function (command, lastCommand) {
+        if (typeof lastCommand === "undefined")
+            lastCommand = false;
+
+        var response = await sendNFCCommand(command, lastCommand, false);
+        if (response.errorCode == 0) {
+            if (typeof response.data !== "undefined")
+                return response.data;
+            else 
+                return;
+        } else {
+            throw Error(response.errorMessage);
+        }
+    };
+
+})(typeof exports === 'undefined'? this['mtnfc']={}: exports);
+
